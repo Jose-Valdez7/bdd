568c2b28ec0cf545b2e45e245c503f5abe9ba845
Jose-Valdez7 <joxzsian@gmail.com>
2024-03-06 00:03:08 -0500

    inserciones a scripts

--- Insercciones/estudiantes.sql
+++ Insercciones/estudiantes.sql
@@ -6,3 +6,27 @@ create table estudiantes(
 	fecha_nacimiento date not null,
 	constraint estudiantes_pk primary key(cedula)
 )
+
+insert into estudiantes (cedula,nombre,apellido,email,fecha_nacimiento)
+values ('1231234567','Eduardo','Mosquera','edu@gmail.com','22/05/1994')
+
+insert into estudiantes (cedula,nombre,apellido,email,fecha_nacimiento)
+values ('1231234568','Jose','Valdez','josx@gmail.com','29/12/1998')
+
+insert into estudiantes (cedula,nombre,apellido,email,fecha_nacimiento)
+values ('1231234569','Mariana','Marrique','mamarri@gmail.com','12/06/1999')
+
+insert into estudiantes (cedula,nombre,apellido,email,fecha_nacimiento)
+values ('1231234566','Angely','Velez','ange@gmail.com','23/03/1996')
+
+insert into estudiantes (cedula,nombre,apellido,email,fecha_nacimiento)
+values ('1231234565','Juan','Andrango','jugo@gmail.com','17/04/2000')
+
+insert into estudiantes (cedula,nombre,apellido,email,fecha_nacimiento)
+values ('1231234564','Hugo','Gutierrez','hugz@gmail.com','15/04/1998')
+
+insert into estudiantes (cedula,nombre,apellido,email,fecha_nacimiento)
+values ('1231234563','Renato','Butilier','buti@gmail.com','19/07/1997')
+
+insert into estudiantes (cedula,nombre,apellido,email,fecha_nacimiento)
+values ('1231234562','Sandra','Vizuete','sand@gmail.com','05/09/1996')
--- Insercciones/productos.sql
+++ Insercciones/productos.sql
@@ -6,3 +6,29 @@ create table productos(
 	stock int not null,
 	constraint productos_pk primary key(codigo)
 )
+
+insert into productos (codigo,nombre,descripcion,precio,stock)
+values (25,'laptop','productos tecnologico de alta gama',900.75,50)
+
+insert into productos (codigo,nombre,descripcion,precio,stock)
+values (26,'mouse','mouse de 400 dp',20.55,100)
+
+insert into productos (codigo,nombre,descripcion,precio,stock)
+values (51,'padmouse','Xl cobbertura total',10.37,150)
+
+insert into productos (codigo,nombre,descripcion,precio,stock)
+values (45,'teclado','teclado raizer mecanico rgb',30.50,25)
+
+insert into productos (codigo,nombre,descripcion,precio,stock)
+values (2,'silla gamer','marca meetion',200.32,35)
+
+insert into productos (codigo,nombre,precio,stock)
+values (4,'CPU',500.00,29)
+
+insert into productos (codigo,nombre,precio,stock)
+values (22,'Pantalla SONY',90.82,23)
+
+insert into productos (codigo,nombre,precio,stock)
+values (11,'Tarjeta de video',324.11,5)
+
+select * from productos
